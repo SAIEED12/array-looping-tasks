bae3e378899b9c002fded4f817f8dcc1d9bdff25
SAIEED12 <abusaieed0@gmail.com>
2026-01-29 13:00:30 +0600

    Task-1

---
+++ task1.js
@@ -0,0 +1,16 @@
+/* Task 1
+Write a JavaScript code to reverse the array colors without using the reverse method.
+
+Input: const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+
+Output:
+
+['orange', 'yellow', 'green', 'blue', 'red'] */
+
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+const reverse = [];
+for(let i=colors.length-1;i>=0;i--)
+{   
+    reverse.push(colors[i]);
+}
+console.log(reverse);
